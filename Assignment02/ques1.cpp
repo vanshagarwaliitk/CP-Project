e140e22ef142f22ac7e3aea6593f74ce3b01298f
vanshagarwaliitk <153419022+vanshagarwaliitk@users.noreply.github.com>
2024-05-28 10:25:58 +0530

    ques1.cpp
    Valid Parentheses

--- Assignment02/ques1.cpp
+++ Assignment02/ques1.cpp
@@ -1,1 +1,38 @@
-# yet to solve
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>s1;
+       
+        bool flag = false;
+        for ( int i = 0; i < s.size() ; i++ ){         
+               if(s[i]=='(' || s[i] =='{' || s[i]=='['){
+                     s1.push(s[i]);
+               }
+               else{
+                 if(s1.empty()){
+                    return false;
+                 }
+                 char a = s1.top();
+                 if(a == '(' && s[i] ==')'){
+                     flag = true;
+                 }
+                 else if(a=='{' && s[i] =='}'){
+                    flag = true;
+                 }
+                 else if(a =='[' && s[i] ==']'){
+                    flag = true;
+                 }
+                 else{
+                    return false;
+                 }
+                 s1.pop();
+               }
+        } 
+               if(s1.empty()){
+                return true;
+               }
+               else{
+                return false;
+               }
+    }         
+};
